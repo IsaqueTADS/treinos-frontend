22a539870e96f455a0a2f574c6173af60058e472
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-05 14:16:45 -0300

    feat: add auth page UI and home page updates

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { redirect } from "next/navigation";
+import { authClient } from "./_lib/auth-client";
+import { headers } from "next/headers";
+
+export default async function Home() {
+  // const session = await authClient.getSession({
+  //   fetchOptions: {
+  //     headers: await headers(),
+  //   },
+  // });
+
+  // if (!session.data?.user) redirect("/auth");
+  return <h1>Home</h1>;
+}
