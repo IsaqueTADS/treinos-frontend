85dfad50272c6a9dd7981d7628d64f24cd79a340
IsaqueTADS <isaqueofc18@gmail.com>
2026-03-06 01:10:38 -0300

    feat: add Orval configuration for API generation and implement custom fetch logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { authClient } from "./_lib/auth-client";
 import { headers } from "next/headers";
+import { getHomeData } from "./_lib/api/fetch-generated";
+import dayjs from "dayjs";
 
 export default async function Home() {
   const session = await authClient.getSession({
@@ -10,5 +12,10 @@ export default async function Home() {
   });
 
   if (!session.data?.user) redirect("/auth");
+
+  const homeData = await getHomeData(dayjs().format("YYYY-MM-DD"));
+
+  console.log(homeData);
+
   return <h1>Home</h1>;
 }
